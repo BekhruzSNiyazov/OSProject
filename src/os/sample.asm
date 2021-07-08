9c3e844f4b264b66e19b64fe901699ec886f759b
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-07-08 23:22:22 +0500

    added src/

---
+++ src/os/sample.asm
@@ -0,0 +1,12 @@
+;****************************************
+; sample.asm
+; A Sample Program
+;
+; Based on "Operating Systems: From 0 to 1" book
+;
+;****************************************
+
+mov eax, 1
+add eax, 1
+
+
